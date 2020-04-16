578dcb4ea12598f77ca476b399ba7ac92662658c
TheElite <47625834+TayChak@users.noreply.github.com>
2020-04-16 02:06:15 +0200

    Add Perfect Cup Project

---
+++ perfectcup_project/footer.php
@@ -0,0 +1,9 @@
+ <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; The Perfect Cup 2020 by <a href="https://github.com/TayChak" target="_blank">Tayeb Chakroun</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
